579edeb4c83c34829026cf8a3cf5a72e2618db8c
Javiisk <javielote94@gmail.com>
2026-06-22 22:57:45 -0600

    Fix typo in setDMPermission method

--- src/commands/Roblox/myinfo.js
+++ src/commands/Roblox/myinfo.js
@@ -58,7 +58,7 @@ export default {
   data: new SlashCommandBuilder()
     .setName('myinfo')
     .setDescription('View you roblox profile info (Use it on DMs)')
-    .setDMpermission (true)
+    .setDMPermission (true)
     .addStringOption(opt =>
       opt.setName('user').setDescription('Your roblox username').setRequired(true)
     ),
