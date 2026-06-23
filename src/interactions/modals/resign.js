4f4399babb0ca5cd6cd07694de17e971a029d0bd
Javiisk <javielote94@gmail.com>
2026-06-23 16:31:00 -0600

    Add resign modal handler to interactions

---
+++ src/interactions/modals/resign.js
@@ -0,0 +1,8 @@
+import { resignDeclineModalHandler } from '../../handlers/resignModals.js';
+
+export default [
+  {
+    name: 'resign_modal',
+    execute: resignDeclineModalHandler.execute,
+  },
+];
