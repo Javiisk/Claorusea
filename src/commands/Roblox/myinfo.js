644276c4a84ab622e20e3bfb5938b680f8834fb8
Javiisk <javielote94@gmail.com>
2026-06-22 23:40:45 -0600

    Add new group 'la vélvoria' to the group list

--- src/commands/Roblox/myinfo.js
+++ src/commands/Roblox/myinfo.js
@@ -16,6 +16,7 @@ const DEFAULT_GROUPS = [
   { id: '14029943', name: 'Empyreúm' },
   { id: '1097260506', name: 'Démoria' },
   { id: '97539052', name: 'Ivaloria' },
+  { id: '35008390', name: 'la vélvoria' },
 ];
 
 function loadGroups() {
