1150727cdb46984a26b1389cdb62408dbf023422
Javiisk <javielote94@gmail.com>
2026-09-24 21:28:36 -0600

    Create shopItems.js

---
+++ src/utils/shopItems.js
@@ -0,0 +1,7 @@
+// src/utils/shopItems.js
+
+// Tools you can buy with /buy, required for /fish and /mine.
+export const SHOP_ITEMS = {
+  pickaxe: { label: 'Pickaxe', price: 500, inventoryKey: 'pickaxe' },
+  fishingrod: { label: 'Fishing Rod', price: 400, inventoryKey: 'fishingrod' },
+};
